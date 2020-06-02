8d1da5092c2b23a8d65f44c051a37d322fb69305
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2020-06-02 09:30:57 -0700

    Rewrite links that break in the publish process.

---
+++ config/link-rewriter.ts
@@ -0,0 +1,14 @@
+/*
+ * @module physics-math
+ * Copyright 2020 by Bob Kerns. Licensed under MIT license.
+ *
+ * Github: https://github.com/BobKerns/physics-math
+ */
+
+/**
+ * URL rewriting for typedoc
+ */
+
+ export = {
+     "^(.*)\.[Mm][Dd]$": "$1.html"
+};
